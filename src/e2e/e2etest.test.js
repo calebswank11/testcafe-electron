4e6d65b692125753ac23c7df221b57ff99ff3c7c
Caleb Swank <calebs@dropitlshotspot.hightouchinc.com>
2019-03-05 13:01:07 -0600

    fix page url in test file

--- src/e2e/e2etest.test.js
+++ src/e2e/e2etest.test.js
@@ -1,6 +1,6 @@
 import { Selector } from "testcafe";
 
-fixture`testing test`.page`./index.html`;
+fixture`testing test`.page`http://localhost:5000/index.html`;
 
 test("test 1", async t => {
   console.log("firing test here");
