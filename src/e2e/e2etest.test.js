b3aaae0f013a548ba82986489af14648710d2fe6
Caleb Swank <calebs@dropitlshotspot.hightouchinc.com>
2019-03-05 12:20:16 -0600

    first commit

---
+++ src/e2e/e2etest.test.js
@@ -0,0 +1,9 @@
+import { Selector } from "testcafe";
+
+fixture`testing test`.page``;
+
+test("test 1", async t => {
+  console.log("firing test here");
+  const test = await Selector("#testingReact").innerText;
+  await t.expect(test).eql("Testing File");
+});
